e4a17e8ac77745d3ea8d266b5384669c3604fbf7
Toni Iannucci <toni.iannucci@gmail.com>
2022-12-30 18:37:02 +0100

    Restructure du back end
    Création de fichiers controllers et routes pour y voir plus clair dans nos fichiers

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const Thing = require('./models/Thing');
 
+const projetRoutes = require('./routes/projet');
 const app = express();
 
 // Permet d'intercepter les requêtes qui contiennent sur json et mettent à disposition sur l'objet requête
@@ -16,11 +17,6 @@ mongoose.connect('mongodb+srv://Toni:Tonic@cluster0.wxdzvma.mongodb.net/?retryWr
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-
-/* **** ROUTES POUR DELETE/POST/UPDATE/GET ***** */
-
-
 // Permet l'accés à notre API
 // D'ajouter les headers aux requêtes
 // D'envoyer des requêtes avec les méthodes
@@ -31,53 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Ajout des données 
-app.post('/', (req, res, next) => {
-  const thing = new Thing({
-    // Va copier les champs dans le corps de la req
-    // Peut aussi le faire comme ça : title: req.body.tittle,
-      /* Test
-    taskName: "Coucou",
-    projectName:"Couco",
-    userId: 29  1,
-    personne:"Toni",
-    tempsTotal:899
-*/
-  ...req.body
-});
-// Sauvegarder les données 
-thing.save()
-  .then(() => res.status(201).json({ message: 'enregistré !'}))
-  .catch(error => res.status(400).json({ error }));
-});
-
-// Modification d'un thing existant 
-app.put('/', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-// Récup les objets 
-app.get('/', (req, res, next) => {
-  // Trouver la liste complète 
-  Thing.find()
-    // Retourne un tableau de tous les things dans la base de données 
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-// Récup un objet spécifique en trouvant son ID
-app.get('/', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-// Supprime un thing de la BDD
-app.delete('/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+// Ici à la place du '/' mettre la route vers le truc 
+// Exemple /api/stuff
+app.use('/',projetRoutes);
 module.exports = app;
---
+++ routes/projet.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const projetCtrl = require('../controller/projet');
+
+
+/* **** ROUTES POUR DELETE/POST/UPDATE/GET ***** */
+
+
+
+
+// Ajout des données
+router.post('/',projetCtrl.createThing);
+  
+  // Modification d'un thing existant 
+  router.put('/',projetCtrl.modifyThing);
+  
+  // Récup les objets 
+  router.get('/',projetCtrl.getAllThings);
+  // Récup un objet spécifique en trouvant son ID
+  router.get('/:id',projetCtrl.getOneThing)
+  
+  // Supprime un thing de la BDD
+  router.delete('/:id',projetCtrl.deleteThing);
+
+
+module.exports = router;
