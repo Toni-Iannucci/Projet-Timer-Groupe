4cfe0fec3b3812913527643174e93137aa732304
Toni Iannucci <toni.iannucci@gmail.com>
2022-12-30 11:46:37 +0100

    Update App.js

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ app.get('/', (req, res, next) => {
 });
 
 // Supprime un thing de la BDD
-app.delete('/api/stuff/:id', (req, res, next) => {
+app.delete('/:id', (req, res, next) => {
   Thing.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
     .catch(error => res.status(400).json({ error }));
